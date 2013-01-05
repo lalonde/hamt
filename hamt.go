9dff82640b1cc020abf3f07bbae8956bcda68044
michael lalonde <michael.j.lalonde@gmail.com>
2013-01-05 16:36:02 -0500

    refactored the interfaces and types.  only able to persist one node.  must implement bit index node to continue

--- hamt.go
+++ hamt.go
@@ -3,6 +3,7 @@ package hamt
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"hash/fnv"
 )
 
@@ -14,104 +15,157 @@ const (
 	keyNotFound = "Key not found"
 )
 
-type Trie struct {
-	root *Node
-	collision map[uint]interface{}
+type node interface {
+	assoc(shift int, hash uint64, key interface{}, value interface{}) node
+	without(shift int, hash uint64, key interface{}) node
+	find(shift int, hash uint64, key interface{}) (value interface{}, err error)
+	depth() int
 }
 
-type Node struct {
-	childBitmap uint64
-	key []byte
+type emptyNode struct {}
+
+type PersistentMap struct {
+	root node
+//	collision map[uint]interface{}
+}
+
+type valueNode struct {
+	key interface{}
+	hash uint64
 	value interface{}
-	children []*Node
-	depth uint
+	shift int
+}
+
+type bitmapNode struct {
+	childBitmap uint64
+	children []*node
+	shift int
+}
+
+func (n *emptyNode) assoc(shift int, hash uint64, key interface{}, val interface{}) node {
+	return &valueNode {key: key, hash: hash, value: val, shift: shift}
+}
+func (n *emptyNode) without(shift int, hash uint64, key interface{}) node {
+	return n
+}
+func (n *emptyNode) find(shift int, hash uint64, key interface{}) (interface{}, error) {
+	return nil, errors.New(keyNotFound)
+}
+func (n *emptyNode) depth() int {
+	return 0
+}
+
+func (n *valueNode) assoc(shift int, hash uint64, key interface{}, val interface{}) node {
+	return &valueNode {key: key, hash: hash, value: val, shift: shift}
+}
+func (n *valueNode) without(shift int, hash uint64, key interface{}) node {
+	return n
+}
+func (n *valueNode) find(shift int, hash uint64, key interface{}) (value interface{}, err error) {
+	if hash == n.hash {
+		value = n.value
+	} else {
+		err = errors.New(keyNotFound)
+	}
+	return value, err
+}
+func (n *valueNode) depth() int {
+	return n.shift
 }
 
 //Shift key hash until leaf with matching key is found or key is not found
-func (t *Trie) Search(key []byte) (node *Node, parent *Node, err error) {
+func (t *PersistentMap) Get(key interface{}) (value interface{}, err error) {
 	//Hash our key and look for it in the root
 	hash := hash(key)
-	parent = t.root
-	node = t.root.children[mask(hash, 0)]
-	
-	//If our value is in the root or the hash does not map to root slot get out fast
-	switch {
-	case node == nil:
-		return node, parent, errors.New(keyNotFound)
-	case node.childBitmap == 0:
-		if bytes.Equal(node.key, key) {
-			return node, nil, nil
-		}
-	}
-
-	//Got here means our slot is sub trie
-	for depth := 1; depth < maxDepth; depth++ {
-		shift := uint(depth * fanoutLog2)
-		pos := bitpos(hash, shift)
-		parent = node
-		if cMap := node.childBitmap; (pos & cMap)  == 0 { //nothing in slot, not found
-			return nil, node, errors.New(keyNotFound)
-		} else {
-			index := node.index(pos)			
-			node = parent.children[index]
-			//Is this a value or map
-			if node.isLeaf() {
-				// Is this who we are looking for?
-				if bytes.Equal(node.key, key) {
-					return node, parent, nil 
-				}
-			} else {
-				continue
-			}
-		}
-	}
-	
-	return nil, parent, errors.New(keyNotFound)
+	value, err = t.root.find(0, hash, key)
+	return
 }
 
-func (t *Trie) Insert(key []byte, value interface{}) (node *Node) {
+//	node = t.root.children[mask(hash, 0)]
+//	
+//	//If our value is in the root or the hash does not map to root slot get out fast
+//	switch {
+//	case node == nil:
+//		return node, parent, errors.New(keyNotFound)
+//	case node.childBitmap == 0:
+//		if bytes.Equal(node.key, key) {
+//			return node, nil, nil
+//		}
+//	}
+//
+//	//Got here means our slot is sub trie
+//	for depth := 1; depth < maxDepth; depth++ {
+//		shift := uint(depth * fanoutLog2)
+//		pos := bitpos(hash, shift)
+//		parent = node
+//		if cMap := node.childBitmap; (pos & cMap)  == 0 { //nothing in slot, not found
+//			return nil, node, errors.New(keyNotFound)
+//		} else {
+//			index := node.index(pos)			
+//			node = parent.children[index]
+//			//Is this a value or map
+//			if node.isLeaf() {
+//				// Is this who we are looking for?
+//				if bytes.Equal(node.key, key) {
+//					return node, parent, nil 
+//				}
+//			} else {
+//				continue
+//			}
+//		}
+//	}
+//	
+//	return nil, parent, errors.New(keyNotFound)
+//}
+
+func (t *PersistentMap) Insert(key interface{}, value interface{}) (n node) {
 	//TODO: miles to go.....
+		
 	hash := hash(key)
-
-	snode, parent, _ := t.Search(key)
-
-	if snode == nil {
-
-		node = NewSub(key, parent.depth +1)
-		node.value = value
-		pos := bitpos(hash, parent.depth * fanoutLog2)
-		parent.childBitmap |= pos
-		if parent.depth == 0 {
-			parent.children[mask(hash, 0)] = node
-		} else {
-			parent.children[parent.index(pos)] = node
-		}
+	n = t.root.assoc(0, hash, key, value)
+	if n.depth() == 0 {
+		t.root = n
 	}
-	return node
+	return n
 }
 
+//	snode, parent, _ := t.Search(key)
+//
+//	if snode == nil {
+//
+//		node = NewSub(key, parent.depth +1)
+//		node.value = value
+//		pos := bitpos(hash, parent.depth * fanoutLog2)
+//		parent.childBitmap |= pos
+//		if parent.depth == 0 {
+//			parent.children[mask(hash, 0)] = node
+//		} else {
+//			parent.children[parent.index(pos)] = node
+//
+//	}
+//	}
+//	return node
+//}
 
 
-func New() *Trie {
-	return &Trie {
-		root: NewSub(nil, 0),
-		collision: make(map[uint]interface{}),
-	}
-}
 
-func NewSub(key []byte, depth uint) *Node {
-	return &Node {
-		childBitmap: 0,
-		key: key,
-		value: nil,
-		children: make([]*Node, fanout),
-		depth: depth,
+func New() *PersistentMap {
+	return &PersistentMap {
+		root: &emptyNode{},
 	}
 }
 
-func hash(b []byte) uint64 {
+func hash(a interface{}) uint64 {
+	buf := new(bytes.Buffer)
 	h := fnv.New64()
-	h.Write(b)
+	_, e := fmt.Fprint(buf, a)
+
+	if e != nil {
+		//doSomethingWithErrors?
+	}
+
+	h.Write(buf.Bytes())
+
 	return h.Sum64()
 }
 
@@ -130,13 +184,6 @@ func bitpos(hash uint64, bshift uint) uint64 {
 	return 1 << mask(hash, bshift)
 }
 
-func (n *Node) index(bit uint64) uint {
+func (n *bitmapNode) index(bit uint64) uint {
 	return popcount_2(n.childBitmap & (bit - 1))
-}
-
-func (n *Node) isLeaf() bool {
-	if n.key != nil && n.childBitmap == 0 {
-		return true
-	} 
-	return false
 }
--- hamt_test.go
+++ hamt_test.go
@@ -6,22 +6,13 @@ import (
 
 func TestEmptyTrie(t *testing.T) {
 	trie := New()
-	root := trie.root
-	if root.childBitmap > 0 {
-		t.Errorf("This new node should be empty ")
+	
+	_,e := trie.Get("EMPTY")
+	
+	if e==nil {
+		t.Errorf("Not finding a key, which we wont find in an empty trie, must return an err")
 	}
 }
-
-func TestSerarchEmpthTrie(t *testing.T) {
-	root := New()
-
-	_,_, err := root.Search([]byte("NOTFOUNDKEY"))
-
-	if err == nil {
-		t.Errorf("Search for a key that is not in trie needs to return not found err")
-	}
-}
-
 
 func TestInsertIntoTrie(t *testing.T) {
 	key1 := "store key1"
@@ -30,28 +21,27 @@ func TestInsertIntoTrie(t *testing.T) {
 	v1 := "value 1"
 	v2 := "value 2"
 
+	t.Logf("Hash of %v = %v", v1, hash(v1))
+	t.Logf("Hash of %v = %v", v2, hash(v2))
+
 	root := New()
 	t.Logf("%v", (1 | 64 | 4))
-	t.Logf("mask: %v", mask(hash([]byte(key1)), 0))
-	t.Logf("bitpos: %v", bitpos(hash([]byte(key1)), 0))
-	s0, p0, e0 := root.Search( []byte(key1) )
-	t.Logf("Search [%v, %v, %v]", s0, p0, e0)
+	t.Logf("mask: %v", mask(hash(key1), 0))
+	t.Logf("bitpos: %v", bitpos(hash(key1), 0))
+	v0, e0 := root.Get( key1 )
+	t.Logf("Get [%v, %v]", v0, e0)
 
-	n1 := root.Insert( []byte(key1), v1 )
+	n1 := root.Insert( key1, v1 )
+	t.Logf("%v", n1)
+	n2 := root.Insert( key2, v2 )
+	t.Logf("%v", n2)
 
-	n2 := root.Insert( []byte(key2), v2 )
+	vg1, e1 := root.Get( key1 )
+	t.Logf("Get [%v, %v]", vg1, e1)
 
-	s1, p1, e1 := root.Search( []byte(key1) )
-	t.Logf("Search [%v, %v, %v]", s1, p1, e1)
-	s2, p2, e2 := root.Search( []byte(key2) )
-	t.Logf("Search [%v, %v, %v]", s2, p2, e2)
+	vg2, e2 := root.Get( key2 )
+	t.Logf("Get [%v, %v]", vg2, e2)
 //	t.Logf("I stored %s for key %s and searching for key %s i got %s", n1.value, key1, key1, s1.value)
 
-	if n1 != s1 {
-		t.Errorf("kv 1 did not store and retrieve stored: %v searched: %v", n1, s1)
-	}
-	
-	if n2 != s2 {
-		t.Errorf("kv 2 did not store and retrieve stored: %v searched: %v", n2, s2)
-	}
+
 }
